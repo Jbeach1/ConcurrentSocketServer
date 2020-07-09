5f40187f5b85f8411dfb817f19d0f6a0f00b078d
Wilson Beach <Joshbeach99@gmail.com>
2020-07-09 17:33:22 -0400

    Make server concurrent

--- Client.java
+++ Client.java
@@ -4,101 +4,100 @@ import java.nio.Buffer;
 
 public class Client {
 
-    public static void main(String args[]) throws IOException, InterruptedException {
+	public static void main(String args[]) throws IOException, InterruptedException {
 
-        System.out.println("Please ensure server is started, enter host:");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String host = reader.readLine();
+		System.out.println("Please ensure server is started, enter host:");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String host = reader.readLine();
 
-        System.out.println("Please enter the port");
-        int port = Integer.parseInt(reader.readLine());
+		System.out.println("Please enter the port");
+		int port = Integer.parseInt(reader.readLine());
 
-        //Get command
-        System.out.println("Please choose a command to run [1-6]");
-        System.out.println("1. date | 2. uptime | 3. free | 4. netstat | 5. who | 6. ps aux");
-        String command = getCommand(reader);
+		// Get command
+		System.out.println("Please choose a command to run [1-6]");
+		System.out.println("1. date | 2. uptime | 3. free | 4. netstat | 5. who | 6. ps aux");
+		String command = getCommand(reader);
 
-        //Get number of simulations
-        System.out.println("How many users would you like to simulate?");
-        System.out.println("Options: 1, 5, 10, 20, 25");
-        int simulations = getSimulations(reader);
+		// Get number of simulations
+		System.out.println("How many users would you like to simulate?");
+		System.out.println("Options: 1, 5, 10, 20, 25");
+		int simulations = getSimulations(reader);
 
-        System.out.println("Command: " + command + " Simulations: " + simulations+"\n");
+		System.out.println("Command: " + command + " Simulations: " + simulations + "\n");
 
-        Thread[] threads = new Thread[simulations];
-        final long[] time = new long[simulations];
-        final String[] server_response = new String[simulations];
+		Thread[] threads = new Thread[simulations];
+		final long[] time = new long[simulations];
+		final String[] server_response = new String[simulations];
 
-        for (int i = 0; i < simulations; i++){
-            server_response[i] = "";
-        }
+		for (int i = 0; i < simulations; i++) {
+			server_response[i] = "";
+		}
 
-        for(int i = 0; i < simulations; i++){
-            final int temp = i;
-            threads[i] = new Thread(() -> {
-                try {
-                    long startTime = System.currentTimeMillis();
-                    //Connect and issue commands & simulations
-                    Socket socket = new Socket(host, port);
-                    PrintWriter input = new PrintWriter(socket.getOutputStream());
-                    BufferedReader server_output = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    System.out.println("Connected, sending commands");
-                    input.println(command);
-                    input.flush();
+		for (int i = 0; i < simulations; i++) {
+			final int temp = i;
+			threads[i] = new Thread(() -> {
+				try {
+					long startTime = System.currentTimeMillis();
+					// Connect and issue commands & simulations
+					Socket socket = new Socket(host, port);
+					PrintWriter input = new PrintWriter(socket.getOutputStream());
+					BufferedReader server_output = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					System.out.println("Connected, sending commands");
+					input.println(command);
+					input.flush();
 
-                    //To do: Build output from what server returns
-                    String server_output_string = "";
-                    while((server_output_string = server_output.readLine()) != null){
-                        server_response[temp] += server_output_string + "\n";
-                    }
+					// To do: Build output from what server returns
+					String server_output_string = "";
+					while ((server_output_string = server_output.readLine()) != null) {
+						server_response[temp] += server_output_string + "\n";
+					}
 
+					// Close connection
+					input.close();
+					socket.close();
+					time[temp] = System.currentTimeMillis() - startTime;
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			});
+		}
 
-                    //Close connection
-                    input.close();
-                    socket.close();
-                    time[temp] = System.currentTimeMillis() - startTime;
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-            });
-        }
+		for (int i = 0; i < simulations; i++) {
+			threads[i].start();
+		}
+		for (int j = 0; j < simulations; j++) {
+			threads[j].join();
+		}
+		for (int i = 0; i < simulations; i++) {
+			System.out.println(server_response[i]);
+		}
+		System.out.println("Connection closed");
+		long total = 0;
+		long avg = 0;
+		for (int i = 0; i < simulations; i++) {
+			System.out.println("Thread " + (i + 1) + ": " + "Turnaround time: " + time[i] + "ms");
+			total = total + time[i];
+		}
+		System.out.println("Total time: " + total + "ms");
+		System.out.println("Average time: " + total / simulations + "ms");
+	}
 
-        for(int i = 0; i < simulations; i++) {
-            threads[i].start();
-        }
-        for(int j = 0; j < simulations; j++) {
-            threads[j].join();
-        }
-        for(int i = 0; i < simulations; i++){
-            System.out.println(server_response[i]);
-        }
-        System.out.println("Connection closed");
-        long total = 0;
-        long avg = 0;
-        for(int i = 0; i < simulations; i++){
-            System.out.println("Thread "+ (i+1) +": "+ "Turnaround time: " + time[i] + "ms");
-            total = total + time[i];
-        }
-        System.out.println("Total time: " + total + "ms");
-        System.out.println("Average time: " + total/simulations + "ms");
-    }
+	private static String getCommand(BufferedReader input) throws IOException {
+		String command = input.readLine();
+		while (!command.matches("[1-6]")) {
+			System.out.println("Please input a number 1-6");
+			command = input.readLine();
+		}
+		return command;
+	}
 
-    private static String getCommand(BufferedReader input) throws IOException {
-        String command = input.readLine();
-        while(!command.matches("[1-6]")){
-            System.out.println("Please input a number 1-6");
-            command = input.readLine();
-        }
-        return command;
-    }
-
-    private static int getSimulations(BufferedReader input) throws IOException {
-        String command = input.readLine();
-        while(!command.matches("(1|5|10|15|20|25)")){
-            System.out.println("Please enter a valid choice: 1, 5, 10, 15, 20, 25");
-            command = input.readLine();
-        }
-        return Integer.parseInt(command);
-    }
+	private static int getSimulations(BufferedReader input) throws IOException {
+		String command = input.readLine();
+		while (!command.matches("(1|5|10|15|20|25)")) {
+			System.out.println("Please enter a valid choice: 1, 5, 10, 15, 20, 25");
+			command = input.readLine();
+		}
+		return Integer.parseInt(command);
+	}
 
 }
--- Server.java
+++ Server.java
@@ -6,74 +6,81 @@ import java.util.List;
 
 public class Server {
 
-    public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please enter host to run on");
-        String host = reader.readLine();
-        System.out.println("Please enter port to run on.");
-        int port = Integer.parseInt(reader.readLine());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter host to run on");
+		String host = reader.readLine();
+		System.out.println("Please enter port to run on.");
+		int port = Integer.parseInt(reader.readLine());
 
-        InetAddress host_address = InetAddress.getByName(host);
+		InetAddress host_address = InetAddress.getByName(host);
 
-        //Make connection
-        ServerSocket server = new ServerSocket(port, 25, host_address);
-        System.out.println("Server started, waiting for a client...");
+		// Make connection
+		ServerSocket server = new ServerSocket(port, 25, host_address);
+		System.out.println("Server started, waiting for a client...");
 
-        try {
-            while(true){
-                Socket socket = server.accept();
+		try {
+			while (true) {
+				Socket socket = server.accept();
+				new Thread(() -> {
+					try {
+						// Get command & iterations from client
+						BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						PrintWriter output = new PrintWriter(socket.getOutputStream());
+						String command;
 
-                //Get command & iterations from client
-                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter output = new PrintWriter(socket.getOutputStream());
-                String command;
+						if ((command = input.readLine()) != null) {
+							// Do the commands
+							switch (command) {
+							case "1":
+								run_cmd("date", output);
+								break;
+							case "2":
+								run_cmd("uptime", output);
+								break;
+							case "3":
+								run_cmd("free", output);
+								break;
+							case "4":
+								run_cmd("netstat", output);
+								break;
+							case "5":
+								run_cmd("who", output);
+								break;
+							case "6":
+								run_cmd("ps aux", output);
+								break;
+							default:
+								System.out.println("Command not recognized");
+							}
+						}
+						// Exit system
+						output.flush();
+						output.close();
+						input.close();
+						socket.close();
 
-                if((command = input.readLine()) != null){
-                    //Do the commands
-                    switch (command) {
-                        case "1":
-                            run_cmd("date", output);
-                            break;
-                        case "2":
-                            run_cmd("uptime", output);
-                            break;
-                        case "3":
-                            run_cmd("free", output);
-                            break;
-                        case "4":
-                            run_cmd("netstat", output);
-                            break;
-                        case "5":
-                            run_cmd("who", output);
-                            break;
-                        case "6":
-                            run_cmd("ps aux", output);
-                            break;
-                        default: System.out.println("Command not recognized");
-                    }
-                }
-                //Exit system
-                output.flush();
-                output.close();
-                input.close();
-                socket.close();
-            }
+					} catch (IOException i) {
+						System.out.println(i);
+					}
 
-        } catch (IOException i) {
-            System.out.println(i);
-        }
-    }
+				}).start();
+			}
+		} catch (IOException i) {
+			System.out.println(i);
+		}
+	}
 
-    private static void run_cmd(String cmd, PrintWriter output) throws IOException {
-        String cmd_output_string = null;
-        Runtime rt = Runtime.getRuntime();
-        Process process = rt.exec(cmd);
-        BufferedReader command_output = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        while((cmd_output_string = command_output.readLine()) != null){
-            output.println(cmd_output_string);
-            System.out.println(cmd_output_string);
-        }
-    }
+	private static void run_cmd(String cmd, PrintWriter output) throws IOException {
+		String cmd_output_string = null;
+		Runtime rt = Runtime.getRuntime();
+		Process process = rt.exec(cmd);
+		BufferedReader command_output = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		while ((cmd_output_string = command_output.readLine()) != null) {
+			output.println(cmd_output_string);
+			System.out.println(cmd_output_string);
+		}
+	}
 
 }
